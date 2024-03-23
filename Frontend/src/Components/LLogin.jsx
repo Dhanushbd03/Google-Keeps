90f7bb66f91b22d18dc51f81a9b8978d95ca8869
dhanushbd03 <dhanushrocks03@gmail.com>
2024-03-23 17:25:07 +0530

    new level

--- Frontend/src/Components/LLogin.jsx
+++ Frontend/src/Components/LLogin.jsx
@@ -3,40 +3,11 @@ import Axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 
 function Login(props) {
-  const [emaillog, setemail] = useState("");
-  const [passwordlog, setpassword] = useState("");
-  const [message, setMessage] = useState("");
-  const color = "red";
-
-  const noacc = () => {
-    props.setreg(false);
-  };
-  const login = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await Axios.post("http://localhost:3000/login", {
-        email: emaillog,
-        password: passwordlog,
-      });
-      console.log(response.data);
-      if (response.data.success) {
-        props.isloggedin(true);
-      } else {
-        setMessage(response.data.error);
-        setColor("red");
-      }
-    } catch (err) {
-      setMessage(err.response.data.error);
-    }
-  };
-  setTimeout(() => {
-    setMessage("");
-  }, 3000);
 
   return (
     <div className="login-form-box" style={{ background: props.color }}>
       <h1>Login</h1>
-      <form onSubmit={login} className="login-form">
+      <form onSubmit={} className="login-form">
         <label htmlFor="email">Email</label>
         <input
           type="email"
@@ -60,7 +31,7 @@ function Login(props) {
         <button style={{ background: props.color }}>Login</button>
       </form>
       <p style={{ color: color }}>{message}</p>
-      <button onClick={noacc}>Don't have a account ?,Register</button>
+      <button onClick={}>Don't have a account ?,Register</button>
     </div>
   );
 }
